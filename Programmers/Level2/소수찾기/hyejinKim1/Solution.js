474b696f2e9cf516efd6f6d719bef2d2b190d9f0
김혜진 <hyejin9203@naver.com>
2024-01-25 21:58:15 +0900

    [240124]Level2_소수찾기

---
+++ Programmers/Level2/소수찾기/hyejinKim1/Solution.js
@@ -0,0 +1,29 @@
+function Solution(numbers){
+  const answer = [];
+  let nums = numbers.split("");
+
+  const isPrime = (num) =>{
+    if(num<=1) return false;
+    for(let i=2; i<Math.sqrt(num); i++){
+      if(num%i == 0) return false;
+    }
+    return true;
+  }
+
+  const per = (arr, fixed) => {
+    if(arr.length >= 1){
+      for(let i=0; i<arr.length; i++){
+        const num = fixed + arr[i];
+        const copyArr = [...arr];
+        copyArr.splice(i, 1);
+        if(!answer.includes(+num) && isPrime(+num)){
+          answer.push(+newNum);
+        }
+        per(copyArr, num);
+      }
+    }
+  }
+
+  per(nums, '');
+  return answer.length;
+}
